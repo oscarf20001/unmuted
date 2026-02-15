b43e6ccfe63459ea294ba333a6fbfb12bf605a91
Oscar <altepommes04@gmail.com>
2026-02-15 16:40:57 +0100

    .htaccess anpassung

--- htmlStructure/footer.php
+++ htmlStructure/footer.php
@@ -1,14 +1,14 @@
 <div id="links">
     <div id="link-payments">
-        <a href="/payment">Zahlungsinformationen</a>
+        <a href="/payment/">Zahlungsinformationen</a>
     </div>
     <div id="link-payments">
-        <a href="/datenschutz">Datenschutz</a>
+        <a href="/datenschutz/">Datenschutz</a>
     </div>
     <div id="link-payments">
-        <a href="/impressum">Impressum</a>
+        <a href="/impressum/">Impressum</a>
     </div>
     <div id="link-payments">
-        <a href="/qa">Q&A</a>
+        <a href="/qa/">Q&A</a>
     </div>
 </div>
